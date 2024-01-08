a37849cf2b119ed65415a56d3f304775f14d81e5
DavidBakalov21 <davidbakalov33@gmail.com>
2024-01-08 21:51:51 +0200

    server and client are now classes

--- ServerKST1/ServerKST1/ServerKST1.cpp
+++ ServerKST1/ServerKST1/ServerKST1.cpp
@@ -2,68 +2,91 @@
 #include <WinSock2.h>
 // Linking the library needed for network communication
 #pragma comment(lib, "ws2_32.lib")
+
+
+class Server
+{
+public:
+	
+
+	int setUP() {
+		
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		{
+			std::cerr << "WSAStartup failed" << std::endl;
+			return 1;
+		}
+		serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (serverSocket == INVALID_SOCKET)
+		{
+			std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+			WSACleanup();
+			return 1;
+		}
+
+
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_addr.s_addr = INADDR_ANY;
+		serverAddr.sin_port = htons(port);
+
+
+		if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return 1;
+		}
+		// Listen for incoming connections
+		if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
+		{
+			std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		clientSocket = accept(serverSocket, nullptr, nullptr);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return 1;
+		}
+	}
+
+	int RecieveSend() {
+		char buffer[1024];
+		memset(buffer, 0, 1024);
+		int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+		if (bytesReceived > 0)
+		{
+			std::cout << "Received data: " << buffer << std::endl;
+			// Send a response back to the client
+			const char* response = "Hello, client! This is the server.";
+			send(clientSocket, response, (int)strlen(response), 0);
+		}
+		// Clean up
+		closesocket(clientSocket);
+		closesocket(serverSocket);
+		WSACleanup();
+		return 0;
+	}
+
+private:
+	WSADATA wsaData;
+	int port = 12345;
+	SOCKET serverSocket;
+	sockaddr_in serverAddr;
+	SOCKET clientSocket;
+};
+
+
 int main()
 {
-	// Initialize Winsock
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
-		std::cerr << "WSAStartup failed" << std::endl;
-		return 1;
-	}
-	// Server configuration
-	int port = 12345;
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (serverSocket == INVALID_SOCKET)
-	{
-		std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
-		WSACleanup();
-		return 1;
-	}
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = INADDR_ANY;
-	serverAddr.sin_port = htons(port);
-	// Bind the socket
-	if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	// Listen for incoming connections
-	if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
-	{
-		std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	std::cout << "Server listening on port " << port << std::endl;
-	// Accept a client connection
-	SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
-	if (clientSocket == INVALID_SOCKET)
-	{
-		std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
-		closesocket(serverSocket);
-		WSACleanup();
-		return 1;
-	}
-	// Receive data from the client
-	char buffer[1024];
-	memset(buffer, 0, 1024);
-	int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-	if (bytesReceived > 0)
-	{
-		std::cout << "Received data: " << buffer << std::endl;
-		// Send a response back to the client
-		const char* response = "Hello, client! This is the server.";
-		send(clientSocket, response, (int)strlen(response), 0);
-	}
-	// Clean up
-	closesocket(clientSocket);
-	closesocket(serverSocket);
-	WSACleanup();
-	return 0;
+	Server serv;
+	serv.setUP();
+	serv.RecieveSend();
+	
 }
