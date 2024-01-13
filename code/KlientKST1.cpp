2f5b36d27ae15fa23efce51b4b633a66aedd9f0f
DavidBakalov21 <davidbakalov33@gmail.com>
2024-01-13 20:10:59 +0200

    put and list

---
+++ code/KlientKST1.cpp
@@ -0,0 +1,161 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#include <string> 
+#include <fstream>
+#include <vector>
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+class Client
+{
+public:
+	int SetUP() {
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		{
+			std::cerr << "WSAStartup failed" << std::endl;
+			return 1;
+		}
+		clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+			WSACleanup();
+			return 1;
+		}
+		serverAddr.sin_family = AF_INET;
+		serverAddr.sin_port = htons(port);
+		InetPton(AF_INET, serverIp, &serverAddr.sin_addr);
+		if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			std::cerr << "Connect failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(clientSocket);
+			WSACleanup();
+			return 1;
+		}
+		return 0;
+	}
+
+	int ReceiveSend(std::string name, std::string choice) {
+
+		if (choice == "get")
+		{
+
+
+			const char* nameS = name.c_str();
+			int commandLength = choice.size();
+			send(clientSocket, (char*)&commandLength, sizeof(int), 0);
+			send(clientSocket, choice.c_str(), choice.size(), 0);
+			int nameLength = name.size();
+			send(clientSocket, (char*)&nameLength, sizeof(int), 0);
+			send(clientSocket, nameS, name.size(), 0);
+			std::streamsize fileSize;
+			if (recv(clientSocket, (char*)&fileSize, sizeof(std::streamsize), 0) == SOCKET_ERROR) {
+				std::cout << "something went wrong when receiving file size" << std::endl;
+				std::cout << WSAGetLastError() << std::endl;
+			}
+
+			std::ofstream outFile(name, std::ios::binary);
+			std::vector<char> fileBuffer(fileSize);
+			if (recv(clientSocket, fileBuffer.data(), fileSize, 0) != SOCKET_ERROR) {
+				outFile.write(fileBuffer.data(), fileSize);
+			}
+			else
+			{
+				std::cout << "something went wrong when receiving file data" << std::endl;
+				std::cout << WSAGetLastError() << std::endl;
+			}
+			outFile.close();
+		}
+		if (choice == "list")
+		{
+			int commandLength = choice.size();
+			send(clientSocket, (char*)&commandLength, sizeof(int), 0);
+			send(clientSocket, choice.c_str(), choice.size(), 0);
+			while (true)
+			{
+				int nameLenght;
+
+				recv(clientSocket, (char*)&nameLenght, sizeof(int), 0);
+				std::vector<char> nameBuffer(nameLenght);
+				recv(clientSocket, nameBuffer.data(), nameLenght, 0);
+				std::string name(nameBuffer.begin(), nameBuffer.end());
+				std::cout << name << std::endl;
+				if (name == "End")
+				{
+					break;
+				}
+			}
+
+
+		}
+		if (choice == "put") {
+			int commandLength = choice.size();
+			send(clientSocket, (char*)&commandLength, sizeof(int), 0);
+			send(clientSocket, choice.c_str(), choice.size(), 0);
+
+			int nameLength = name.size();
+			const char* nameS = name.c_str();
+			send(clientSocket, (char*)&nameLength, sizeof(int), 0);
+			send(clientSocket, nameS, name.size(), 0);
+			std::string filepath = "C:\\Users\\Давід\\source\\repos\\Lab1\\KlientKST1\\KlientKST1\\client\\" + name;
+			std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+			std::streamsize fileSize = file.tellg();
+			file.seekg(0, std::ios::beg);
+			send(clientSocket, (char*)&fileSize, sizeof(std::streamsize), 0);
+			std::vector<char> fileBuffer(fileSize);
+			if (file.read(fileBuffer.data(), fileSize)) {
+				send(clientSocket, fileBuffer.data(), fileSize, 0);
+			}
+
+			file.close();
+
+
+
+		}
+
+		closesocket(clientSocket);
+		WSACleanup();
+		return 0;
+	}
+
+private:
+	WSADATA wsaData;
+	int port = 12345;
+	PCWSTR serverIp = L"127.0.0.1";
+	SOCKET clientSocket;
+	sockaddr_in serverAddr;
+};
+
+
+int main()
+{
+	Client cl;
+
+
+	while (true)
+	{
+		cl.SetUP();
+		std::cout << "Enter:" << std::endl;
+		std::string choice;
+
+		std::string text;
+		//std::cout << "Enter a choice: ";
+		std::getline(std::cin, choice);
+		if (choice == "Q")
+		{
+			break;
+		}
+		std::getline(std::cin, text);
+		// Initialize Winsock
+
+
+		cl.ReceiveSend(text, choice);
+		std::cout << "ended" << std::endl;
+
+	}
+	//}
+
+	return 0;
+}
---
+++ code/ServerKST1.cpp
@@ -0,0 +1,152 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+
+// Linking the library needed for network communication
+#pragma comment(lib, "ws2_32.lib")
+class Server
+{
+public:
+	int setUP() {
+
+		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		{
+			std::cerr << "WSAStartup failed" << std::endl;
+			return 1;
+		}
+		serverSocket = socket(AF_INET, SOCK_STREAM, 0);//AF_INET2 The Internet Protocol version 4 (IPv4)address family.
+		//SOCK_STREAM-wo-way, connection-based byte streams with an OOB data transmission mechanism
+		if (serverSocket == INVALID_SOCKET)
+		{
+
+			std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
+			WSACleanup();
+			return 1;
+		}
+		serverAddr.sin_family = AF_INET; // indicates te IP version (like iPv4)
+		serverAddr.sin_addr.s_addr = INADDR_ANY; // it recieves messages from any IP
+		serverAddr.sin_port = htons(port); // port number on which server listens 
+
+		//need to bind socket to local address
+		if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) //
+		{
+			std::cerr << "Bind failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return 1;
+		}
+		// Listen for incoming connections
+		if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
+		{
+			std::cerr << "Listen failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return 1;
+		}
+		clientSocket = accept(serverSocket, nullptr, nullptr);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			std::cerr << "Accept failed with error: " << WSAGetLastError() << std::endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return 1;
+		}
+		return 0;
+	}
+	int ReceiveSend() {
+
+
+		int choiceLenght;
+		recv(clientSocket, (char*)&choiceLenght, sizeof(int), 0);
+		std::vector<char> choiceBuffer(choiceLenght);
+		recv(clientSocket, choiceBuffer.data(), choiceLenght, 0);
+		std::string choice(choiceBuffer.begin(), choiceBuffer.end());
+
+
+		if (choice == "get") {
+			int nameLenght;
+			recv(clientSocket, (char*)&nameLenght, sizeof(int), 0);
+			std::vector<char> nameBuffer(nameLenght);
+			recv(clientSocket, nameBuffer.data(), nameLenght, 0);
+			std::string name(nameBuffer.begin(), nameBuffer.end());
+			std::string filepath = "C:\\Users\\Давід\\source\\repos\\Lab1\\ServerKST1\\ServerKST1\\database\\" + name;
+
+			std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+			std::streamsize fileSize = file.tellg();
+			file.seekg(0, std::ios::beg);
+			send(clientSocket, (char*)&fileSize, sizeof(std::streamsize), 0);
+			std::vector<char> fileBuffer(fileSize);
+			if (file.read(fileBuffer.data(), fileSize)) {
+				send(clientSocket, fileBuffer.data(), fileSize, 0);
+			}
+
+			file.close();
+		}
+		if (choice == "list")
+		{
+			for (const auto& entry : std::filesystem::directory_iterator("C:\\Users\\Давід\\source\\repos\\Lab1\\ServerKST1\\ServerKST1\\database")) {
+				int lenght = entry.path().string().size();
+				send(clientSocket, (char*)&lenght, sizeof(int), 0);
+				send(clientSocket, entry.path().string().c_str(), lenght, 0);
+			}
+			std::string end = "End";
+			int endLength = end.size();
+			send(clientSocket, (char*)&endLength, sizeof(int), 0);
+			send(clientSocket, end.c_str(), endLength, 0);
+		}
+		if (choice == "put")
+		{
+
+			int nameLenght;
+			recv(clientSocket, (char*)&nameLenght, sizeof(int), 0);
+			std::vector<char> nameBuffer(nameLenght);
+			recv(clientSocket, nameBuffer.data(), nameLenght, 0);
+			std::string name(nameBuffer.begin(), nameBuffer.end());
+
+			std::streamsize fileSize;
+			if (recv(clientSocket, (char*)&fileSize, sizeof(std::streamsize), 0) == SOCKET_ERROR) {
+				std::cout << "something went wrong when receiving file size" << std::endl;
+				std::cout << WSAGetLastError() << std::endl;
+			}
+
+			std::ofstream outFile("C:\\Users\\Давід\\source\\repos\\Lab1\\ServerKST1\\ServerKST1\\database\\" + name, std::ios::binary);
+			std::vector<char> fileBuffer(fileSize);
+			if (recv(clientSocket, fileBuffer.data(), fileSize, 0) != SOCKET_ERROR) {
+				outFile.write(fileBuffer.data(), fileSize);
+			}
+			else
+			{
+				std::cout << "something went wrong when receiving file data" << std::endl;
+				std::cout << WSAGetLastError() << std::endl;
+			}
+			outFile.close();
+		}
+		closesocket(clientSocket);
+		closesocket(serverSocket);
+		WSACleanup();
+		return 0;
+	}
+private:
+	WSADATA wsaData;
+	int port = 12345;
+	SOCKET serverSocket;
+	sockaddr_in serverAddr;
+	SOCKET clientSocket;
+};
+int main()
+{
+	Server serv;
+
+
+	while (true)
+	{
+		std::cout << "started" << std::endl;
+		serv.setUP();
+		serv.ReceiveSend();
+		std::cout << "ended" << std::endl;
+
+	}
+}
